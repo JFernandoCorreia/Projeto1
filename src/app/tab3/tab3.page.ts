363d0c0514ab65d4cc00f848aa99a536f0213520
JFernando <almo_fernando@hotmail.com>
2024-04-04 20:26:07 -0300

    first commit

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -5,8 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page { 
 
-  constructor() {}
+  constructor() { }
 
+  // //alunos = [
+  //   {
+  //     'nome': 'Aluno 1',
+  //     'matrícula': 1111111
+  //     'avatar': '../../assets/avatar/'
+  //   },
+
+  //   {'nome': 'Aluno 1',
+  //   'matrícula': 222222
+  //   'avatar': '../../assets'},
+
+  //   {'nome': 'Aluno 1',
+  //   'matrícula': 33333333
+  //   'avatar': '../../assets'},
+
+  //   {'nome': 'Aluno 1',
+  //   'matrícula': 4444444
+  //   'avatar': '../../assets'}
+
+  // ];
 }
+
+
+
